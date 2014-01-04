77b5c33ed3fc838b792a463d440263845a2d1565
steadshot <steadshot@gmail.com>
2014-01-04 20:56:45 +0100

    better piece class
    better piece class

--- game.py
+++ game.py
@@ -7,9 +7,6 @@ playfield = [[0 for i in range(20)] for j in range(10)]
 start_x = 50
 start_y = 100
 
-piece_position = (3, 0)
-
-current_piece = piece.getPiece()
 
 def drawField(screen):
 	"""draws a field, big whoop"""
@@ -21,10 +18,10 @@ def drawField(screen):
 				pygame.draw.rect(screen, (0, 0, 0), (start_x + i * block_size, start_y + j * block_size, block_size, block_size), 0)
 
 def drawPiece(screen):
-	for i, line in enumerate(current_piece):
+	for i, line in enumerate(piece.getPiece()):
 		for j, x in enumerate(line):
 			if x == 1:
-				pygame.draw.rect(screen, (255, 255, 255), (piece_position[0] * block_size + start_x + i * block_size, piece_position[1] * block_size + start_y + j * block_size, block_size, block_size), 0)
+				pygame.draw.rect(screen, (255, 255, 255), (piece.piece_position[0] * block_size + start_x + i * block_size, piece.piece_position[1] * block_size + start_y + j * block_size, block_size, block_size), 0)
 
 
 def drawBorder(screen):
@@ -34,58 +31,19 @@ def drawBorder(screen):
 				(start_x, start_y + 20 * block_size), (start_x, start_y)], 1)
 
 
-
-def rotatePiece(screen, rotation):
-	global current_piece
-	if rotation == "cw":
-		piece.rotate('cw')
-		if not outOfBounds(piece_position, piece.getPiece()):
-			current_piece = piece.getPiece()
-		else:
-			piece.rotate('ccw')
-	elif rotation == "ccw":
-		piece.rotate('ccw')
-		if not outOfBounds(piece_position, piece.getPiece()):
-			current_piece = piece.getPiece()
-		else:
-			piece.rotate('cw')
-
-	drawField(DISPLAYSURF)
-	drawPiece(DISPLAYSURF)
-	drawBorder(DISPLAYSURF)
-
-
-def movePiece(screen, key):
-	global piece_position
-	if key == "DOWN":
-		new_piece_position = (piece_position[0], piece_position[1] + 1)
-	elif key == "UP":
-		new_piece_position = (piece_position[0], piece_position[1] - 1)
-	elif key == "LEFT":
-		new_piece_position = (piece_position[0] - 1, piece_position[1])
-	elif key == "RIGHT":
-		new_piece_position = (piece_position[0] + 1, piece_position[1])
-	if not outOfBounds(new_piece_position, current_piece):
-		piece_position = new_piece_position
+def refresh(screen):
 	drawField(screen)
 	drawPiece(screen)
 	drawBorder(screen)
-	
-def outOfBounds(position, piece):
-	for i, line in enumerate(piece):
-		for j,x in enumerate(line):
-			if x == 1:
-				if (not i + position[0] in range(10)) or (not j + position[1] in range(20)):
-					return True
-	return False
+
+
+
 
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('nomires - where tetris goes to die')
-drawField(DISPLAYSURF)
-drawPiece(DISPLAYSURF)
-drawBorder(DISPLAYSURF)
+refresh(DISPLAYSURF)
 while True:
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -93,22 +51,27 @@ while True:
 			sys.exit()
 		if event.type == KEYDOWN:
 			if event.key == K_z:
-				rotatePiece(DISPLAYSURF, 'ccw')
+				piece.rotatePiece(DISPLAYSURF, 'ccw')
+				refresh(DISPLAYSURF)
 			if event.key == K_x:
-				rotatePiece(DISPLAYSURF, 'cw')
+				piece.rotatePiece(DISPLAYSURF, 'cw')
+				refresh(DISPLAYSURF)
 
 			if event.key == K_DOWN:
 
-				movePiece(DISPLAYSURF, "DOWN")
+				piece.movePiece("DOWN")
+				refresh(DISPLAYSURF)
 			if event.key == K_UP:
 				#movePiece(DISPLAYSURF, "UP")
 				pass
 
 
 			if event.key == K_RIGHT:
-				movePiece(DISPLAYSURF, "RIGHT")
+				piece.movePiece("RIGHT")
+				refresh(DISPLAYSURF)
 			if event.key == K_LEFT:
-				movePiece(DISPLAYSURF, "LEFT")
+				piece.movePiece("LEFT")
+				refresh(DISPLAYSURF)
 			if event.key == K_RETURN:
 				pygame.quit()
 				sys.exit()
--- piece.py
+++ piece.py
@@ -26,11 +26,51 @@ t_piece = [
 	]
 
 rotation_state = 0
+piece_position = (3, 0)
 
 piece = t_piece
 
 def getPiece():
 	return piece[rotation_state]
+
+
+current_piece = getPiece()
+
+def outOfBounds(position, piece):
+	for i, line in enumerate(piece):
+		for j,x in enumerate(line):
+			if x == 1:
+				if (not i + position[0] in range(10)) or (not j + position[1] in range(20)):
+					return True
+	return False
+
+def movePiece(key):
+	global piece_position
+	if key == "DOWN":
+		new_piece_position = (piece_position[0], piece_position[1] + 1)
+	elif key == "UP":
+		new_piece_position = (piece_position[0], piece_position[1] - 1)
+	elif key == "LEFT":
+		new_piece_position = (piece_position[0] - 1, piece_position[1])
+	elif key == "RIGHT":
+		new_piece_position = (piece_position[0] + 1, piece_position[1])
+	if not outOfBounds(new_piece_position, getPiece()):
+		piece_position = new_piece_position
+
+def rotatePiece(screen, rotation):
+	global current_piece
+	if rotation == "cw":
+		rotate('cw')
+		if not outOfBounds(piece_position, getPiece()):
+			current_piece = getPiece()
+		else:
+			rotate('ccw')
+	elif rotation == "ccw":
+		rotate('ccw')
+		if not outOfBounds(piece_position, getPiece()):
+			current_piece = getPiece()
+		else:
+			rotate('cw')
 
 def rotate(direction):
 	global rotation_state
